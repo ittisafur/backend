9f72a7f528ce5fa3a449054aae56ba987a1fb995
ittisafur <ittisafur@gmail.com>
2024-12-31 10:54:31 +0600

    Configs to point

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://endpoints.ittisafur.com'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
