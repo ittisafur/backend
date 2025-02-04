c22ded518df6219e161f8eb2f2e71745a273cfe9
ittisafur <ittisafur@gmail.com>
2025-02-04 16:53:19 +0600

    Update Cloudinary

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,20 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::cors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
